659908e341e50b2118ac2c95fdbda07369982856
Osazee Isere <oz1.concepts@gmail.com>
2021-06-25 04:03:03 +0100

    Add project files.

---
+++ MInterfaceHierachy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MInterfaceHierachy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
